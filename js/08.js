0911b7521864d477c0a66ba2872bdffd6c68fb21
Gerardo Vazquez <neowhatup@gmail.com>
2024-02-18 23:16:52 -0600

    intro_js

---
+++ js/08.js
@@ -0,0 +1,12 @@
+
+//* Concatenacion y templeString
+
+const nombre = 'Juan';
+const email = 'correo@correo.com';
+
+//* Concatenación
+console.log("Nombre Cliente: " + nombre + " Email: " + email);
+
+
+//* Template Strings - String Literals
+console.log(`Nombre Cliente: ${nombre} Email: ${email}`);
