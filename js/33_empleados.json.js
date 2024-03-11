649d29aeceb4d428e8ea3a76c4234a8344a1257b
Gerardo Vazquez <neowhatup@gmail.com>
2024-03-11 15:56:50 -0600

    ingresado

--- js/33_empleados.json.js
+++ js/33_empleados.json.js
@@ -4,17 +4,18 @@ async function obtenerEmpleados() {
 
     const archivo = 'empleados.json';
 
-    //* fetch(archivo)
-    //*    .then( resultado => resultado.json())
-    //*     .then( datos => {
-    //*         // console.log(datos.empleados);
+     fetch(archivo)
+        .then( resultado => resultado.json())
+        .then( datos => {
+        console.log(datos.empleados);
 
-    //*         const { empleados } = datos;
-    //*        console.log(empleados);
-    //*     });
+            const { empleados } = datos;
+          console.log(empleados);
+        });
 
     const resultado = await fetch(archivo);
     const datos = await resultado.json();
     console.log(datos);
 }
 obtenerEmpleados();
+
